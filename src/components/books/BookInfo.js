b33823cdc0840bf0840ab62acb8dd37871b4933d
yogbharat1 <41539033+yogbharat1@users.noreply.github.com>
2018-10-09 20:29:38 +0530

    UI fix

--- src/components/books/BookInfo.js
+++ src/components/books/BookInfo.js
@@ -12,35 +12,23 @@ const BookInfo = (props) => {
     }
 
     return (
-        <div className="col-sm-6 col-md-4 col-lg-4 mt-4 mb-4">
-                <div className="card card-inverse card-info">
-                    <img 
-                        className="card-img-top"
-                        alt={bookDetails.best_book.title}
-                        src={bookDetails.best_book.image_url} 
-                        style={{
-                            width: "100%",
-                            height: "5em",
-                            ObjectFit: "cover"
-                        }}/>
-                    <div className="card-block">
-                        <h5 className="card-title mt-3">{bookDetails.best_book.author.name}</h5>
-                        
-                        <div className="card-text">
-                            {titleTruncate}
-                        </div>
-                    </div>
-                    <div className="card-footer">
-                        <Rating 
-                        initialRating={bookDetails.average_rating}
-                        readonly                        
-                        /> <small>{bookDetails.average_rating}</small>
-                        <a href={`https://www.goodreads.com/book/show/${
-                    bookDetails.best_book.id
-                }`} className="btn btn-info float-right btn-sm">More Info >></a>
-                    </div>
-                </div>
+        <div className="col-sm-6 col-md-4 col-lg-4 mt-2 mb-2">
+            <div className="card flex-md-row mb-2 shadow-sm h-md-150">
+            <div className="card-body d-flex flex-column align-items-start">
+               <strong className="d-inline-block mb-2 text-primary">{titleTruncate}</strong>
+               <h6 className="mb-0">
+                  <a className="text-dark" href="#">{bookDetails.best_book.author.name}</a>
+               </h6>
+               <div className="mb-1 text-muted small">Publication Year: {bookDetails.original_publication_year}</div>
+               <div>
+                <span className="mt-2 d-block small">Average Rating: <mark>{bookDetails.average_rating}</mark></span>
+               <Rating initialRating={bookDetails.average_rating} readonly />
+               </div>
+               <a className="btn btn-outline-primary btn-sm" role="button" href={`https://www.goodreads.com/book/show/${bookDetails.best_book.id}`}>Continue reading</a>
             </div>
+            <img className="card-img-right flex-auto d-none d-lg-block" alt={bookDetails.best_book.author.name} src={bookDetails.best_book.image_url}  style={{width: "150px", height: "200px"}}/>
+         </div>                
+        </div>
       );
 }
 
